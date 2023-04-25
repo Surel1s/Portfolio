2ee8c696b7f8a1fbb857d5a993f08b538f5f3f49
Lukas Jokubauskas <melinakislukas@gmail.com>
2023-04-25 20:47:44 +0300

    Main

---
+++ script.js
@@ -0,0 +1,19 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".work-section");
+    var otherSections = document.querySelectorAll(".contact");
+
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
